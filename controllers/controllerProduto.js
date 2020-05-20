a0906c0b1b55c0e0e83f48a228c6839f402e0a1b
Erika <liraerika00@gmail.com>
2020-05-19 22:44:25 -0300

    CRUD de produto

--- controllers/controllerProduto.js
+++ controllers/controllerProduto.js
@@ -1,8 +1,19 @@
 const models = require('../app/models');
 
-const getProducts = async (req, res) => {
+const createProduct = async (req, res) => {
+  try {
+    const products = await models.Produto.create(req.body);
+    return res.status(201).json({
+      products,
+    });
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+}
+
+const getAllProducts = async (req, res) => {
 	try {
-	const produtos = await models.Produto.findAll(
+	const products = await models.Produto.findAll(
 		{
 		  include: [
 		    {
@@ -12,12 +23,68 @@ const getProducts = async (req, res) => {
 		  ]
 		}
 	);
-	return res.status(200).json({ produtos });
+	return res.status(200).json({ products });
 	} catch (error) {
 	return res.status(500).send(error.message);
 	}
 }
 
+const getProductById = async (req, res) => {
+  try {
+    const { productsId } = req.params;
+    const products = await models.Produto.findOne({
+      where: { id: productsId },
+      include: [
+        {
+          model: models.Categoria,
+          as: 'categorias'
+        }
+      ]
+    });
+    if (products) {
+      return res.status(200).json({ products });
+    }
+    return res.status(404).send('Produto with the specified ID does not exists');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+const updateProduct = async (req, res) => {
+  try {
+    const { productsId } = req.params;
+    const [ updated ] = await models.Produto.update(req.body, {
+      where: { id: productsId }
+    });
+    if (updated) {
+      const updatedProduto = await models.Produto.findOne({ where: { id: productsId } });
+      return res.status(200).json({ products: updatedProduto });
+    }
+    throw new Error('Produto não foi encontrado!');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { productsId } = req.params;
+    const deleted = await models.Produto.destroy({
+      where: { id: productsId }
+    });
+    if (deleted) {
+      return res.status(204).send("Produto deleted");
+    }
+    throw new Error("Produto not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
 module.exports = {
-  getProducts
+	createProduct,
+ 	getAllProducts,
+ 	getProductById,
+ 	updateProduct,
+ 	deleteProduct
 }
--- routes/produtos.js
+++ routes/produtos.js
@@ -3,6 +3,10 @@ var router = express.Router();
 const controllers = require('../controllers/controllerProduto');
 
 /* GET users listing. */
-router.get('/', controllers.getProducts);
+router.post('/', controllers.createProduct);
+router.get('/', controllers.getAllProducts);
+router.get('/:productsId', controllers.getProductById);
+router.put('/:productsId', controllers.updateProduct);
+router.delete('/:productsId', controllers.deleteProduct);
 
 module.exports = router;
