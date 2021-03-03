7c492b86256a2a53d0cec2c196c8a49e8197d55f
Erika Lira <liraerika00@gmail.com>
2021-03-03 03:14:30 -0300

    Updates

---
+++ precache-manifest.8134b427705e8dc3fcb69fe69c744de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296acaa14275009adbed7ebfe892c00f",
+    "url": "/CRUDFullStack/index.html"
+  },
+  {
+    "revision": "98d9c50edce734372210",
+    "url": "/CRUDFullStack/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5438136733ff768c193e",
+    "url": "/CRUDFullStack/static/js/2.1efaee9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUDFullStack/static/js/2.1efaee9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d9c50edce734372210",
+    "url": "/CRUDFullStack/static/js/main.a63bfd37.chunk.js"
+  },
+  {
+    "revision": "cb42abaf0131b06d11cb",
+    "url": "/CRUDFullStack/static/js/runtime-main.37a52a8e.js"
+  }
+]);
